cf4d928d12403a1b64ad9c863a80b3c1196a9f33
chodon sass <chodon@users.noreply.github.com>
2017-11-13 15:27:32 -0800

    Update moveAndRename.batch.sh

--- moveAndRename.batch.sh
+++ moveAndRename.batch.sh
@@ -3,7 +3,7 @@
 #SBATCH --partition=savio2_htc
 #SBATCH --account=co_rosalind
 #SBATCH --qos=rosalind_htc2_normal
-#SBATCH --mail-user=chodon@berkeley.edu
+#SBATCH --mail-user=username@berkeley.edu
 #SBATCH --mail-type=ALL
 #SBATCH --time=72:00:00 
 
