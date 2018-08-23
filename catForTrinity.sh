c60ffdc0a1570815b1133a87bd1a03c53b7dcf92
chodon sass <chodon@users.noreply.github.com>
2018-08-23 11:20:58 -0700

    Update catForTrinity.sh

--- catForTrinity.sh
+++ catForTrinity.sh
@@ -1,17 +1,17 @@
-bzcat CSJM001A_1.fq.bz2 CSJM001B_1.fq.bz2 CSJM001C_1.fq.bz2 CSJM001D_1.fq.bz2 CSJM001E_1.fq.bz2 CSJM001F_1.fq.bz2 CSJM001N_1.fq.bz2 CSJM001A_u.fq.bz2 CSJM001B_u.fq.bz2 CSJM001C_u.fq.bz2 CSJM001D_u.fq.bz2 CSJM001E_u.fq.bz2 CSJM001F_u.fq.bz2 CSJM001N_u.fq.bz2 > ../cmon/cmon_1.fq
+bzcat CSJM001A_1.fq.bz2 CSJM001B_1.fq.bz2 CSJM001C_1.fq.bz2 CSJM001D_1.fq.bz2 CSJM001E_1.fq.bz2 CSJM001F_1.fq.bz2 CSJM001N_1.fq.bz2  > ../cmon/cmon_1.fq
 bzcat CSJM001A_2.fq.bz2 CSJM001B_2.fq.bz2 CSJM001C_2.fq.bz2 CSJM001D_2.fq.bz2 CSJM001E_2.fq.bz2 CSJM001F_2.fq.bz2 CSJM001N_2.fq.bz2  > ../cmon/cmon_2.fq
 
-bzcat CSJM001G_1.fq.bz2 CSJM001H_1.fq.bz2 CSJM001I_1.fq.bz2 CSJM001J_1.fq.bz2 CSJM001K_1.fq.bz2 CSJM001L_1.fq.bz2 CSJM001G_u.fq.bz2 CSJM001H_u.fq.bz2 CSJM001I_u.fq.bz2 CSJM001J_u.fq.bz2 CSJM001K_u.fq.bz2 CSJM001L_u.fq.bz2 > ../cama/cama_1.fq
+bzcat CSJM001G_1.fq.bz2 CSJM001H_1.fq.bz2 CSJM001I_1.fq.bz2 CSJM001J_1.fq.bz2 CSJM001K_1.fq.bz2 CSJM001L_1.fq.bz2 > ../cama/cama_1.fq
 bzcat CSJM001G_2.fq.bz2 CSJM001H_2.fq.bz2 CSJM001I_2.fq.bz2 CSJM001J_2.fq.bz2 CSJM001K_2.fq.bz2 CSJM001L_2.fq.bz2 > ../cama/cama_2.fq
 
 
-cat CSJM002E_1.fq CSJM002E_u.fq > ../cumb/cumb_1.fq
-cp CSJM002E_2.fq > ../cumb/cumb_2.fq
+cp CSJM002E_1.fq ../cumb/cumb_1.fq
+cp CSJM002E_2.fq ../cumb/cumb_2.fq
 
 
-bzcat CSJM001M_1.fq.bz2 CSJM001M_u.fq.bz2 > ../calb/calb_1.fq
+bzcat CSJM001M_1.fq.bz2 > ../calb/calb_1.fq
 bzcat CSJM001M_2.fq.bz2 > ../calb/calb_2.fq
 
-cat CSJM002A_1.fq CSJM002B_1.fq CSJM002C_1.fq CSJM002D_1.fq CSJM002F_1.fq CSJM002G_1.fq CSJM002H_1.fq CSJM002I_1.fq CSJM002J_1.fq CSJM002K_1.fq CSJM002L_1.fq CSJM002M_1.fq CSJM002A_u.fq CSJM002B_u.fq CSJM002C_u.fq CSJM002D_u.fq CSJM002F_u.fq CSJM002G_u.fq CSJM002H_u.fq CSJM002I_u.fq CSJM002J_u.fq CSJM002K_u.fq CSJM002L_u.fq CSJM002M_u.fq > ../cven/cven_1.fq
+cat CSJM002A_1.fq CSJM002B_1.fq CSJM002C_1.fq CSJM002D_1.fq CSJM002F_1.fq CSJM002G_1.fq CSJM002H_1.fq CSJM002I_1.fq CSJM002J_1.fq CSJM002K_1.fq CSJM002L_1.fq CSJM002M_1.fq > ../cven/cven_1.fq
 cat CSJM002A_2.fq CSJM002B_2.fq CSJM002C_2.fq CSJM002D_2.fq CSJM002F_2.fq CSJM002G_2.fq CSJM002H_2.fq CSJM002I_2.fq CSJM002J_2.fq CSJM002K_2.fq CSJM002L_2.fq CSJM002M_2.fq > ../cven/cven_2.fq
 
